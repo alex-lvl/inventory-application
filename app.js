18a4fb81aa7e4d93ba360125034a20323fa4382f
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-06-08 01:30:17 -0400

    added helmet and compression

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 var formRouter = require('./routes/form');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
+app.use(helmet());
 require('dotenv').config();
 
 //Set up mongoose connection
@@ -28,6 +31,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
